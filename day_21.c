f95d5a432054e30e32b6bec92b025fa7f22fdb0b
Pihu-Agrawal <pihuagrawal038@gmail.com>
2026-03-16 23:03:22 +0530

    Add files via upload

---
+++ day_21.c
@@ -0,0 +1,61 @@
+/*Problem: Create and Traverse Singly Linked List
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+
+Output:
+- Print the result
+
+Example:
+Input:
+5
+10 20 30 40 50
+
+Output:
+10 20 30 40 50*/
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node *next;
+};
+int main(){
+    int n,value;
+
+    struct node *head=NULL,*new_node=NULL,*temp=NULL;
+
+    //input number of nodes
+    scanf("%d",&n);
+
+    for(int i=0;i<n;i++){
+        printf("enter the value of %d node:",i+1);
+        scanf("%d",&value);
+        new_node=(struct node*)malloc(sizeof(struct node));
+        new_node->data=value;
+        new_node->next=NULL;
+
+        if(head==NULL){
+            head=new_node;
+            temp=new_node;
+        }
+        else{
+            temp->next=new_node;
+            temp=new_node;
+        }
+
+        
+    }
+    temp=head;
+    while(temp!=NULL){
+        printf("%d ",temp->data);
+        temp=temp->next;
+    }
+
+    return 0;
+
+}
+
+
+
+
